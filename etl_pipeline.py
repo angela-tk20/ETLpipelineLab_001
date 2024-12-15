17d5d020353195b9cfbd149cac8f0f35a856374b
anlintk <tenkorangangela20@gmail.com>
2024-12-15 19:27:40 +0000

    feature: implements transform stage to merge dataframes, handle missing values ,convert datatypes, calculate for revenue and create new columns for date

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -12,3 +12,25 @@ def extract():
 
     return orders_df, products_df
 
+
+def transform(orders_df, products_df):
+    merged_df = pd.merge(orders_df, products_df, on='product_id', how='left')
+
+    # Handle missing values
+    merged_df = merged_df.dropna()
+    # Convert data types
+    merged_df['order_date'] = pd.to_datetime(merged_df['order_date'])
+    merged_df['quantity'] = merged_df['quantity'].astype(int)
+    merged_df['price'] = merged_df['price'].astype(float)
+
+
+    quantities = merged_df['quantity'].to_numpy()
+    prices = merged_df['price'].to_numpy()
+    revenue = np.multiply(quantities, prices)
+
+    merged_df['revenue'] = revenue
+
+    merged_df['order_date'] = pd.to_datetime(merged_df['order_date'])
+    merged_df['year'] = merged_df['order_date'].dt.year
+    merged_df['month'] = merged_df['order_date'].dt.month
+    merged_df['day'] = merged_df['order_date'].dt.day
